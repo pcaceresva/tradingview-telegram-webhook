621e0f924d7fe02a7e7f25a995571acccafdb609
pcaceresva <pcaceresva@gmail.com>
2025-07-31 17:02:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ CHAT_IDS = {
     "MACD40-2": "-1002579121705",   # Canal para indicador MACD4
     "EMA+MACD40-1": "-1002411599382",   # Canal para indicador EMA+MACD
     "EMA+MACD40-2": "-1002411599382",   # Canal para indicador EMA+MACD
-    "SUPERTRENDRSI40-1": "-1002813953373"  # Canal para indicador Supertrend-RSI
+    "SUPERTRENDRSI40-1": "-1002813953373",  # Canal para indicador Supertrend-RSI
     "SUPERTRENDRSI40-1": "-1002813953373"  # Canal para indicador Supertrend-RSI
 }
 
