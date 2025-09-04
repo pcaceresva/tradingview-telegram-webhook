219139b9f9acc5f7eef050178360dc181ec9e5b2
pcaceresva <pcaceresva@gmail.com>
2025-09-04 10:23:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ CHAT_IDS = {
     "CRUCEEMA40-1": "-1002411599382",   # Canal para indicador EMA+MACD
     "CRUCEEMA40-2": "-1002411599382",   # Canal para indicador EMA+MACD
     "SUPERTRENDRSI40-1": "-1002813953373",  # Canal para indicador Supertrend-RSI
-    "SUPERTRENDRSI40-2": "-1002813953373"  # Canal para indicador Supertrend-RSI
+    "SUPERTRENDRSI40-2": "-1002813953373",  # Canal para indicador Supertrend-RSI
+    "SUPERTRENDRSI40-3": "-1002813953373"  # Canal para indicador Supertrend-RSI
 }
 
 @app.route("/", methods=["POST"])
@@ -57,3 +58,4 @@ def webhook():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
